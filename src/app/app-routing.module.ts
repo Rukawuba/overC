c5b28eba1266e181d2f85ee0984f6967a9b98120
Rob_U <rcu3@njit.edu>
2022-12-26 16:27:00 -0500

    Updated from Dec 26

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CountryComponent } from './country/country.component';
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full' },
+  {path: 'login',component: LoginComponent },
+  {path: 'home',component: HomeComponent },
+  {path: 'country',component: CountryComponent },
+  {path: 'login',component: LoginComponent },
+  {path: '**',component: LoginComponent },
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
